987ab72a9693f5a5ff64e5ecd2b41fabe04956cd
Jonathanlau92 <jonathan@excide.co>
2020-02-22 09:19:51 +0800

    Route sign in path and write seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin1 = User.create(email: 'Jonlau@gmail.com', password: 'password')
