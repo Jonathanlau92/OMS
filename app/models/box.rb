6ace6310fc8f95510eb35b2fe221ac087a4234a3
Jonathanlau92 <jonathan@excide.co>
2020-03-07 12:53:36 +0800

    Nested form views

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,4 +1,5 @@
 class Box < ApplicationRecord
 	has_many :products
+  accepts_nested_attributes_for :products, reject_if: :all_blank, allow_destroy: true
   belongs_to :order
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  belongs_to :boxes
+  belongs_to :box
 
   has_one_attached :product_image
 end
